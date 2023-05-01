64871cc1ebafb687cba5c2ba5abd6e36e80db796
medhidev <grimalmedhi4@gmail.com>
2023-05-02 00:17:04 +0200

    edit de mise en forme

--- script/main.js
+++ script/main.js
@@ -1,4 +1,11 @@
 function ConvertToArduino()
 {
     var userInsert = document.querySelector("#userContent").value;
+    var output = document.querySelector("#code").value;
+    switch (userInsert)
+    {
+        case "hello":
+            output = "salut";
+            break;
+    }
 }
